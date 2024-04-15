c5303f50b80de56451a90bedfd6ce6a35ea1de14
yookeunbyul <91243651+yookeunbyul@users.noreply.github.com>
2024-04-15 15:13:13 +0900

    💄 Design: Image 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import profilePic from "/public/hello.jpg";
 
 export default function Home() {
   return (
     <>
       <h2>Welcome</h2>
       Hello, WEB!
+      <br />
+      <Image src={profilePic} alt="" />
     </>
   );
 }
