42e5a18a0c500b6ae578d8af42318d5575a2de5d
yookeunbyul <91243651+yookeunbyul@users.noreply.github.com>
2024-04-15 17:34:54 +0900

    ✨ Feat: api로 title, body 불러오기

--- src/app/read/[id]/page.tsx
+++ src/app/read/[id]/page.tsx
@@ -1,8 +1,10 @@
-export default function Read({ params }: { parmas: { id: string } }) {
+export default async function Read({ params }: { parmas: { id: string } }) {
+  const resp = await fetch(`http://localhost:9999/topics/${params.id}`);
+  const topic = await resp.json();
   return (
     <>
-      <h2>Read</h2>
-      paramters {params.id}
+      <h2>{topic.title}</h2>
+      {topic.body}
     </>
   );
 }
